5f0021a66e14115753151c1b98bd4087f2934b08
Yueh Ning <ning.84386@gmail.com>
2021-06-12 22:58:46 +0800

    happy apple

---
+++ 123.py
@@ -0,0 +1,8 @@
+number = 30
+apple = 0.2
+happy = False
+black = '黑色'
+print(number)
+print(apple)
+print(happy)
+print(black)
---
+++ hy.py
@@ -0,0 +1,3 @@
+height = input('請輸入身高: ')
+weight = input('請輸入體重: ')
+print('你的身高和體重是: ', height, weight)
